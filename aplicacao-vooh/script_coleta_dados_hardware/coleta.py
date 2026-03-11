a935d43f227b4b1135bfa3df865e129a2aff9770
Mar_Hiro <Ma.Hiro.Yogi.Carvalho@gmail.com>
2026-03-11 11:23:58 -0300

    Enhance data capture with additional metrics

--- aplicacao-vooh/script_coleta_dados_hardware/coleta.py
+++ aplicacao-vooh/script_coleta_dados_hardware/coleta.py
@@ -9,9 +9,13 @@ INTERVALO = 3
 
 def captura_dados():
     final_io = psutil.disk_io_counters()
+
     pular = "\n" * 2
+
     while True: 
+
         porcentagem_uso_da_cpu = psutil.cpu_percent(INTERVALO)
+        cpu_freq = psutil.cpu_freq()
 
         porcentagem_uso_do_disco = psutil.disk_usage("/").percent 
         initial_io = psutil.disk_io_counters()
@@ -28,31 +32,43 @@ def captura_dados():
         memoria_percentual = (memoria_disponivel / memoria_total) * 100
 
         timestamp_atual = datetime.now()
-        timestamp_formatado = timestamp_atual.isoformat() # formato iso: YYYY-MM-DDThh:mm:ss
+
+        timestamp_formatado = timestamp_atual.isoformat()
 
         conteudo_csv = {
             "usuario": os.getlogin(),
             "porcentagem_uso_da_cpu": porcentagem_uso_da_cpu,
+            "Frequência Atual": int(cpu_freq.current),
+            "Frequência Mínima": int(cpu_freq.min),
+            "Frequência Máxima": int(cpu_freq.max),
+
             "porcentagem_uso_do_disco": porcentagem_uso_do_disco,
+            "read_rate_Bps": int(read_rate_Bps),
+            "write_rate_Bps": int(write_rate_Bps),
+
             "memoria_percentual": memoria_percentual,
-            "read_rate_Bps": read_rate_Bps,
-            "write_rate_Bps": write_rate_Bps,
+            "memoria_total": int(memoria_total),
+            "memoria_disponivel": int(memoria_disponivel),
+
             "data": timestamp_formatado,
         }
         inserir_arquivo_csv(conteudo_csv)    
         time.sleep(INTERVALO)    
         final_io = psutil.disk_io_counters()
 
+
 def inserir_arquivo_csv(dados):
+
     print(dados)
+
     nome_arquivo = f"coleta_{date.today()}.csv"
+
     with open(nome_arquivo, mode='a', newline='') as arquivo_csv:
         writer = csv.DictWriter(arquivo_csv, fieldnames=dados.keys(), delimiter=';')
+
         if arquivo_csv.tell() == 0:
             writer.writeheader()
+
         writer.writerow(dados)
 
-
-
-#começar captura
 captura_dados()
