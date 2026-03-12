d848f348959358b66aed4d7008b2ead569f62c4e
PedroHenriqueCCOK <pedro.holiveira@sptech.school>
2026-03-12 09:26:11 -0300

    Código de cadastro responsivo, alguns ajustes na index, porém não funcional no bd

--- aplicacao-vooh/web_teste/teste_1/public/src/models/usuarioModel.js
+++ aplicacao-vooh/web_teste/teste_1/public/src/models/usuarioModel.js
@@ -3,14 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsers, username, email, nome_completo FROM Users WHERE email = '${email}' AND passcode = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-function autenticar_user(username, senha) {
-    var instrucaoSql = `
-        SELECT idUsers, username, email, nome_completo FROM Users WHERE username = '${username}' AND passcode = '${senha}';
+        SELECT nome, email FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -29,6 +22,5 @@ function cadastrar(fkEmpresa, nome, email, senha) {
 
 module.exports = {
     autenticar,
-    autenticar_user,
     cadastrar,
 };
